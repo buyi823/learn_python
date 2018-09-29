2bd643282ebd7ee857775999e2897eae3c226798
309521086@qq.com <309521086@qq.com>
2018-09-29 18:25:24 +0800

    some practices

---
+++ some_practices/password2.py
@@ -0,0 +1,10 @@
+def main():
+    pwd = input('what is your password')
+    if pwd == 'apple':
+        print('Logging on...')
+    else:
+        print('incorrect password')
+    print('All done')
+
+
+    
