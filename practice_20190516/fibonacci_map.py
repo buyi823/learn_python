7416a5107ce73d074384e26f6b87ff890ad4d666
buyi823 <309521086@qq.com>
2019-05-20 19:18:52 +0800

    array practice

--- practice_20190516/fibonacci_map.py
+++ practice_20190516/fibonacci_map.py
@@ -5,7 +5,7 @@ n = int(input('Enter the number of terms: '))
 
 def fibonacci(count):
     fib_list = [0,1]
-    any(map(lambda _: fib_list.append(sum(fib_list[-2:])),range(2,count)))
+    any((map(lambda _: fib_list.append(sum(fib_list[-2:])),range(2,count))))
     return fib_list[:count]
 
 print(fibonacci(n))
