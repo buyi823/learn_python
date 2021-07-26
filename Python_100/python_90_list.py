3641b880d6d454025a296095322978e227089395
BlaineXu <blaine.xu@kaiostech.com>
2021-07-26 18:20:26 +0800

    python 100 practice

--- Python_100/python_90_list.py
+++ Python_100/python_90_list.py
@@ -3,10 +3,10 @@
 # By Blaine 2021-07-14 15:07
 # list
 
-matrix = [[1,2,3], [4,5,6], [7,8,9]]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(matrix)
 print(matrix[1])
-col2 = [row[1] for row in matrix] #get a  column from a matrix  
+col2 = [row[1] for row in matrix]  # get a  column from a matrix
 print(col2)
 coleven = [row[1] for row in matrix if row[1] % 2 == 0]
 print(coleven)
