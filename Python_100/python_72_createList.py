9674cb76d6c101187ab6fdade3c9dc3a86745f3c
BlaineXu <blaine.xu@kaiostech.com>
2021-09-14 18:26:50 +0800

    practice

---
+++ Python_100/python_72_createList.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# By Blaine 2021-09-14 17:36
+# create a list
+
+if __name__ == '__main__':
+    ptr = []
+    for i in range(5):
+        num = int(input('Please input a number:\n'))
+        ptr.append(num)
+         
+    print(ptr)
