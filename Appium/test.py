504693e05fb3634a213b4eba7d7b8f34d260ecdb
BlaineXu <blaine.xu@kaiostech.com>
2021-10-29 18:27:30 +0800

    appium practice

--- Appium/test.py
+++ Appium/test.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 # By Blaine 2021-10-27 10:52
-# 
+# Appium example
 
 import os
 import subprocess
 from appium import webdriver
+import time
 
-# print(os.popen('adb devices -l'))
-# print(list(os.popen('adb devices -l').readlines()))
-a = os.popen('adb devices -l').readlines()
-print(a)
-# os.system("adb devices -l")
+calculator = {
+    'platformName': 'Android',
+    'deviceName': 'Redmi Note 8 Pro',
+    'platformVersion': '11',
+    'appPackage': 'com.miui.calculator',
+    'appActivity': '.cal.CalculatorActivity'
+}
+
+driver = webdriver.Remote('http://localhost/wd/hub', calculator)
+time.sleep(2)
+
+def cal():
+    driver.find_element_by_id().click()
+    
+
+cal()
+
