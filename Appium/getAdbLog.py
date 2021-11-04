4ddafa4cbbc300c93d7508688bc4c678f3e63855
blaine <blaine.xu@kaiostech.com>
2021-11-04 18:35:05 +0800

    practice

--- Appium/getAdbLog.py
+++ Appium/getAdbLog.py
@@ -1,12 +1,51 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-# By Blaine 2021-11-02 17:13
-# 抓adb log
+# By Blaine 2021-11-04 17:05
+# 循环抓adb log
 
 import os, time
 
-print('请确保连接好手机。')
+# 判断手机是否连接
+def isDeviceConnect():
+    # 将获取的设备信息保存在列表中，列表中结尾是"\n"
+    # 以结尾的回车来判断
+    devices = list(os.popen('adb devices').readlines())
+    if devices[1] == '\n':
+        print('Not found any device. Please check "adb devices".')
+        return False
+    elif len(devices) > 3: 
+        print('More than one device.')
+        return False 
+    else:
+        print(os.popen('adb devices').read())
+        return True
 
-deviceInfo = os.popen('adb wait-for-device').read()
-# if deviceInfo in
-print(deviceInfo)
+
+# 获取LOG
+def getAdbLog():
+    os.popen('adb root')
+    # interval: 抓log间隔时间,用户输入次数,单位秒
+    # times：循环抓LOG次数，用户输入次数
+    for i in range(times):
+        # 放在当前目录了, 以抓log时间命名
+        os.popen('adb logcat > log_{time}.txt'.format(time=time.strftime('%Y%m%d%H%M%S', time.localtime())))
+        print(f'第{i+1}次获取抓取成功......{time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime())}')
+        i += 1
+        # 抓取LOG的间隔时间可以设置
+        print(f'waiting {interval}s.....')
+        time.sleep(interval)
+
+
+if __name__ == '__main__':
+    
+    if isDeviceConnect():
+        # abs() 正数 
+        interval = abs(int(input('请输入抓log间隔时间(单位秒),请输入正整数后回车： ')))
+        times = abs(int(input('请输入您想抓取log的次数，请输入正整数后回车： ')))
+        getAdbLog()
+
+# 生成以当前时间命名的文件
+# uuid_str = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime())
+# tmp_file_name = '%s.txt' % uuid_str
+# with open(tmp_file_name, 'a') as f:
+#     f.write("{}\n\n".format(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))))
--- Appium/get_Android_info.py
+++ Appium/get_Android_info.py
@@ -1,43 +1,72 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-# By Blaine 2021-10-29 17:11
+# By Blaine 2021-10-29 17:11 
 # 获取Android信息
+# Update code 2021-11-04-15:49:17
 
-import os
+import os, time
 from types import GetSetDescriptorType
+import csv
+
+def isDeviceConnect():
+    # 将获取的设备信息保存在列表中，列表中结尾是"\n"
+    devices = list(os.popen('adb devices').readlines())
+    if devices[1] == '\n':
+        print('Not found any device. Please check "adb devices".')
+        return False
+    elif len(devices) > 3: 
+        print('==More than one device.==')
+        return False 
+    else:
+        print(os.popen('adb devices').read())
+        return True
+
 
 # adb shell getprop
+def getDeviceInfo():
 
-os.popen('adb root')
-deviceName = os.popen('adb shell getprop ro.product.model').read()
-print('deviceName: ' + deviceName)
+    os.popen('adb root')
+    deviceName = os.popen('adb shell getprop ro.product.model').read()
+    print('deviceName: ' + deviceName)
 
-platformVersion = os.popen('adb shell getprop ro.build.version.release').read()
-print('platformVersion: ' + platformVersion)
+    platformVersion = os.popen('adb shell getprop ro.build.version.release').read()
+    print('platformVersion: ' + platformVersion)
 
-device_product = os.popen('adb shell getprop ro.product.name').read()
-print(f'device: {device_product}')
+    device_product = os.popen('adb shell getprop ro.product.name').read()
+    print(f'device: {device_product}')
 
-# 全信息显示分辨率信息
-# device_displays = os.popen('adb shell dumpsys window displays').read()
-# print(device_displays)
+    # 全信息显示分辨率信息
+    # device_displays = os.popen('adb shell dumpsys window displays').read()
+    # print(device_displays)
 
-device_physicalSize = os.popen('adb shell wm size').read()
-print(device_physicalSize)
+    device_physicalSize = os.popen('adb shell wm size').read()
+    print(device_physicalSize)
 
-# 查看屏幕密度
-device_density = os.popen('adb shell wm density').read()
-print(device_density)
+    # 查看屏幕密度
+    device_density = os.popen('adb shell wm density').read()
+    print(device_density)
+
+    device_battery = os.popen('adb shell dumpsys battery').read()
+    print(device_battery)
+
+    appPackage_Activity = os.popen('adb shell "dumpsys activity | grep "mResume""').read().rstrip()
+    print(appPackage_Activity.lstrip()) #将字符前面空格删除掉
+
+# 将信息导出到csv   还不知道怎么实现
+# def deviceInfo_csv():
+#     # start_time = time.strftime('%Y-%m-%d %H:%M:%S %z', time.localtime())
+#     # csv_data = [start_time]
+#     path = "deviceInfo.csv"
+#     with open(path, 'wb') as f:
+#         csv_write = csv.writer(f)
+#         csv_head = ["deviceName", "platformVersion", "platformVersion", 
+#         "device_product", "device_physicalSize", 'device_density', 'device_battery']
+#         csv_write.writerows(csv_head)
 
 
-device_battery = os.popen('adb shell dumpsys battery').read()
-print(device_battery)
 
-appPackage_Activity = os.popen('adb shell dumpsys activity | findstr "mResume"').read().rstrip()
-print(appPackage_Activity.lstrip()) #将字符前面空格删除掉
-
-# 有个想法把一个手机的基础信息导出到excel， 如何去实现？ 2011/11/2 18：26
-
-
+if __name__ == '__main__':
+    if isDeviceConnect():
+        getDeviceInfo()
 
 
