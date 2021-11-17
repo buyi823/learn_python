71aa0037ea82be150e3857ec1548c7b317018302
BlaineXu <blaine.xu@kaiostech.com>
2021-11-17 18:49:21 +0800

    learn

--- Appium/Appium_test.py
+++ Appium/Appium_test.py
@@ -6,14 +6,24 @@
 from appium import webdriver
 import time
 from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.support.wait import WebDriverWait
 
 # desired capability
+# 需要注意的是默认输入中文无效，但是不会报错，需要在“启动代码”中增加两个参数。
+# desired_caps['unicodeKeyboard'] = True
+# desired_caps['resetKeyboard'] = True 
+
+
+
+
 calculator = {
     'platformName': 'Android',
     'deviceName': 'Redmi Note 8 Pro',
     'platformVersion': '11',
     'appPackage': 'com.miui.calculator',
-    'appActivity': '.cal.CalculatorActivity'
+    'appActivity': '.cal.CalculatorActivity',
+    'unicodeKeyboard': True,
+    'resetKeyboard': True
 }
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', calculator)
@@ -24,7 +34,6 @@ print(driver.get_window_size())
 # 截图
 driver.get_screenshot_as_file("d:\\logs\\appium_screenshot.png")
 print("screenshot path: d:\\logs")
-
 
 # appium 教程参考 https://www.cnblogs.com/liuhui0308/p/12033199.html
 
@@ -55,8 +64,6 @@ else:
 #         print("Network: 4 Data Only")
 #     case 6:
 #         print("Network: 6 All network on")
-
-
 
 
 # 从一个元素滑动到另一个元素，直到页面自动停止。
@@ -138,6 +145,12 @@ def test():
     # 打印当前界面名
     print(f"Current activity: {driver.current_activity}")
     
+    
+    # 需要获取元素的位置和大小的时候使用
+    buttonEqual = driver.find_element_by_id("com.miui.calculator:id/btn_equal_s")
+    print(buttonEqual.location)
+    print(buttonEqual.size)
+    
     # 安装应用
     # driver.install_app("path")
     # # 卸载应用
@@ -145,20 +158,52 @@ def test():
     # # 检查应用是否安装
     # driver.is_app_installed(app_id)
     
+    # E.G. 
+    # if driver.is_app_installed("com.tencent.android.qqdownloader"):
+    #     driver.remove_app("com.tencent.android.qqdownloader")
+    # else:
+    #     driver.install_app("d:\yingyongbao.apk")
+    
+    
     # 在脚本内启动其他应用
-    driver.start_activity('appPackage', 'appActivity')
+    # driver.start_activity('appPackage', 'appActivity')
     
-    # 关闭应用   这种关闭呢，是关闭驱动对象，同时关闭所有关联的应用，关闭后我们是无法使用脚本操作应用的。
-    driver.quit()
+    # # 关闭应用   这种关闭呢，是关闭驱动对象，同时关闭所有关联的应用，关闭后我们是无法使用脚本操作应用的。
+    # driver.quit()
     
-    # 我们只想关闭当前操作的应用，不关闭驱动对象，我们就可以使用driver.close_app()方法。
-    driver.close_app()
+    # # 我们只想关闭当前操作的应用，不关闭驱动对象，我们就可以使用driver.close_app()方法。
+    # driver.close_app()
     
-    # 重置应用  相当于“恢复出厂默认值”的效果
-    driver.reset()
+    # # 重置应用  相当于“恢复出厂默认值”的效果
+    # driver.reset()
     
-    # 将应用置于后台
-    driver.background_app(seconds)
+    # # 将应用置于后台
+    # driver.background_app(seconds)
+    
+    # 我们在使用脚本的时候，可能会由于网络、服务器处理、电脑等原因，我们想要找的元素没有加载出来，这个时候如果直接定位就可能会报错。
+    # 这个时候我们就可以设置元素等待了。
+    # 什么叫元素等待呢？
+    # 就是WebDriver定位页面元素时如果没有找到，就会在指定时间内一直等待的过程。
+    # 元素等待一共分为两种类型：显示等待和隐式等待。
+    # driver.implicitly_wait(5)
+    
+    # 显示等待就是为需要等待的元素分别设置不同的值。
+    # 等待元素加载指定的时长，超出时长抛出TimeoutException异常。
+    # WebDriverWait(driver, timeout, poll_frequency=0.5)：
+    # 参数分别是驱动对象、超时时长、检测间隔时间，检测间隔时间默认是0.5秒。
+    # wait.until(method)：参数为lambda查找元素表达式。
+    # 需要导入 from selenium.webdriver.support.wait import WebDriverWait
+    
+    # 在5秒钟内，每1秒在《设置》中的“返回”按钮，如果找到则点击，如果找不到则观察对应错误信息。
+    # 第一种
+    # driver.find_element_by_id('com.android.settings:id/search').click()
+    # wait = WebDriverWait(driver, 25, 5)
+    # 第二种
+    # back_button = WebDriverWait(driver, 5, 1).until(lambda x: x.find_element_by_xpath("//*[@content-desc='收起']"))
+    # back_button.click()
+    # 第三种
+    # WebDriverWait(driver, 5, 1).until(lambda x: x.find_element_by_xpath("//*[@content-desc='收起']")).click()
+    
     
     
 plus()
--- Appium/get_Android_info.py
+++ Appium/get_Android_info.py
@@ -3,11 +3,19 @@
 # By Blaine 2021-10-29 17:11 
 # 获取Android信息
 # Update code 2021-11-04-15:49:17
+# 折腾了一天都没有把导出excel的问题搞定。。。。。。 By Blaine 2021-11-17 18:46  
+
 
 import os, time
 from types import GetSetDescriptorType
 import csv
+import xlwt
+import pandas as pd
+from pandas import DataFrame
+import numpy as np
 
+
+# Check that your device is connected properly
 def isDeviceConnect():
     # 将获取的设备信息保存在列表中，列表中结尾是"\n"
     devices = list(os.popen('adb devices').readlines())
@@ -22,52 +30,114 @@ def isDeviceConnect():
         return True
 
 
+# Get DUT information
 # adb shell getprop
 def getDeviceInfo():
 
     os.popen('adb root')
-    deviceName = os.popen('adb shell getprop ro.product.model').read()
+    deviceName = os.popen('adb shell getprop ro.product.model').read().rstrip()
     print('deviceName: ' + deviceName)
 
-    platformVersion = os.popen('adb shell getprop ro.build.version.release').read()
+    platformVersion = os.popen('adb shell getprop ro.build.version.release').read().rstrip()
     print('platformVersion: ' + platformVersion)
 
-    device_product = os.popen('adb shell getprop ro.product.name').read()
+    device_product = os.popen('adb shell getprop ro.product.name').read().rstrip()
     print(f'device: {device_product}')
 
     # 全信息显示分辨率信息
     # device_displays = os.popen('adb shell dumpsys window displays').read()
     # print(device_displays)
 
-    device_physicalSize = os.popen('adb shell wm size').read()
+    device_physicalSize = os.popen('adb shell wm size').read().rstrip()
     print(device_physicalSize)
 
     # 查看屏幕密度
-    device_density = os.popen('adb shell wm density').read()
+    device_density = os.popen('adb shell wm density').read().rstrip()
     print(device_density)
 
-    device_battery = os.popen('adb shell dumpsys battery').read()
+    device_battery = os.popen('adb shell dumpsys battery').read().rstrip()
     print(device_battery)
 
     appPackage_Activity = os.popen('adb shell "dumpsys activity | grep "mResume""').read().rstrip()
     print(appPackage_Activity.lstrip()) #将字符前面空格删除掉
+    
+    info_list = [deviceName, platformVersion, device_product, device_physicalSize, device_density,
+                 device_battery, appPackage_Activity]
+    
+    return info_list
 
-# 将信息导出到csv   还不知道怎么实现
-# def deviceInfo_csv():
-#     # start_time = time.strftime('%Y-%m-%d %H:%M:%S %z', time.localtime())
-#     # csv_data = [start_time]
-#     path = "deviceInfo.csv"
-#     with open(path, 'wb') as f:
-#         csv_write = csv.writer(f)
-#         csv_head = ["deviceName", "platformVersion", "platformVersion", 
-#         "device_product", "device_physicalSize", 'device_density', 'device_battery']
-#         csv_write.writerows(csv_head)
+
+# use pandas
+def pandas_exportInfo():
+    # 目前可以生成标题正确的EXCEL，不知道怎么写入数据
+    title = ["Device Name", "Platform Version", "Device Product", 
+            "Device Physical Size", "Device Density", "Device Battery", "appPackage Activity"]
+    df1 = pd.DataFrame(columns=title)
+    df1.to_excel("PhoneInfo.xlsx", sheet_name='Phone Information', index=False, header=True)
+    
+    
+
+# Export DUT information to Excel
+# xlwt最大只支持256列
+# 可以生成EXCEL，写入的循环有问题
+def create_file(content):
+    # init style
+    style_head = xlwt.XFStyle()
+    # init font
+    font = xlwt.Font()
+    font.name = "Calibri"
+    font.bold = True
+    # index must digit
+    font.colour_index = 1
+    # init backgroud pictrue
+    bg = xlwt.Pattern()
+    # May be: NO_PATTERN, SOLID_PATTERN, or 0x00 through 0x12
+    bg.pattern = xlwt.Pattern.SOLID_PATTERN
+    # May be: 8 through 63. 0 = Black, 1 = White, 2 = Red, 3 = Green, 4 = Blue, 5 = Yellow, 
+    # 6 = Magenta, 7 = Cyan, 16 = Maroon, 17 = Dark Green, 18 = Dark Blue, 19 = Dark Yellow , almost brown), 
+    # 20 = Dark Magenta, 21 = Teal, 22 = Light Gray, 23 = Dark Gray
+    bg.pattern_fore_colour = 4
+    
+    # set font
+    style_head.font = font
+    # set background
+    style_head.pattern = bg
+    
+    # create a excel
+    excel = xlwt.Workbook(encoding='utf-8')
+    # add worksheet
+    sheet = excel.add_sheet("The Phone Information")
+    # xlwt中是行和列都是从0开始计算的
+    first_col_1 = sheet.col(1)
+    first_col_3 = sheet.col(7)
+    # 设置创建时间宽度
+    first_col_1.width = 256 * 50
+    # 设置存储路径列宽度
+    first_col_3.width = 256 * 100
+    # title info
+    head = ["Device Name", "Platform Version", "Device Product", 
+            "Device Physical Size", "Device Density", "Device Battery", "appPackage Activity"]
+    # create title
+    for index, value in enumerate(head):
+        sheet.write(0, index, value, style_head)
+     
+    # 循环写入   （这里有问题总报错 ）
+    # for index, value_list in enumerate(content, 1):
+    #     for i, value in enumerate(value_list):
+    #         sheet.write(index, i, value)
+    
+    file_name = "PhoneInfo"
+    excel.save("./%s.xls" % file_name)
+    return file_name
+    
 
 
 
 if __name__ == '__main__':
     if isDeviceConnect():
         getDeviceInfo()
+        # create_file(getDeviceInfo())
+        pandas_exportInfo()
     
 
 
